89e12238b644e5670328fe3ce6f52225ebe05c25
mosiahvuxs <moisessrocha@gmail.com>
2012-09-04 16:53:03 -0300

    .

---
+++ src/br/com/zapeat/site/dao/BannerDAO.java
@@ -0,0 +1,34 @@
+package br.com.zapeat.site.dao;
+
+import br.com.topsys.database.TSDataBaseBrokerIf;
+import br.com.topsys.database.factory.TSDataBaseBrokerFactory;
+import br.com.topsys.util.TSUtil;
+import br.com.zapeat.site.model.BannerModel;
+
+public class BannerDAO {
+	
+	public BannerModel pesquisar(BannerModel model) {
+
+		TSDataBaseBrokerIf broker = TSDataBaseBrokerFactory.getDataBaseBrokerIf();
+
+		StringBuilder sql = new StringBuilder("SELECT B.ID, B.IMAGEM, B.TAXA_PRIORIDADE, B.QDO_CLIQUES, B.FLAG_ATIVO FROM PUBLIC.BANNERS B, FORNECEDORES F, TIPOS_BANNERS TB WHERE B.FORNECEDOR_ID = F.ID AND B.TIPO_BANNER_ID = TB.ID AND B.FLAG_ATIVO AND DATE_TRUNC ('DAY', B.INICIO) >= CURRENT_DATE AND DATE_TRUNC('DAY', B.FIM) <= CURRENT_DATE");
+
+		if (!TSUtil.isEmpty(model) && !TSUtil.isEmpty(model.getFornecedorModel()) && !TSUtil.isEmpty(model.getFornecedorModel().getId())) {
+
+			sql.append(" AND CF.FORNECEDOR = ?");
+		}
+
+		sql.append(" ORDER BY RANDOM() LIMIT 1");
+
+		broker.setSQL(sql.toString());
+
+		if (!TSUtil.isEmpty(model) && !TSUtil.isEmpty(model.getFornecedorModel()) && !TSUtil.isEmpty(model.getFornecedorModel().getId())) {
+
+			broker.set(model.getFornecedorModel().getId());
+		}
+
+		return (BannerModel) broker.getObjectBean(BannerModel.class, "id", "imagem", "taxaPrioridade", "qdoCliques", "flagAtivo");
+
+	}
+
+}
---
+++ src/br/com/zapeat/site/dao/FornecedorDAO.java
@@ -0,0 +1,33 @@
+package br.com.zapeat.site.dao;
+
+import java.util.List;
+
+import br.com.topsys.database.TSDataBaseBrokerIf;
+import br.com.topsys.database.factory.TSDataBaseBrokerFactory;
+import br.com.zapeat.site.model.FornecedorModel;
+
+public class FornecedorDAO {
+
+	@SuppressWarnings("unchecked")
+	public List<FornecedorModel> pesquisarHome() {
+
+		TSDataBaseBrokerIf broker = TSDataBaseBrokerFactory.getDataBaseBrokerIf();
+
+		broker.setPropertySQL("fornecedordao.pesquisarHome");
+
+		return broker.getCollectionBean(FornecedorModel.class, "id", "logoMarca", "nomeFantasia");
+
+	}
+	
+	@SuppressWarnings("unchecked")
+	public List<FornecedorModel> pesquisarTopGeral() {
+
+		TSDataBaseBrokerIf broker = TSDataBaseBrokerFactory.getDataBaseBrokerIf();
+
+		broker.setPropertySQL("fornecedordao.pesquisarTopGeral");
+
+		return broker.getCollectionBean(FornecedorModel.class, "id", "logoMarca", "nomeFantasia");
+
+	}
+
+}
