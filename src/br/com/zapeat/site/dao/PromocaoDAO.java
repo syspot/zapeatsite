d9a8d41ef56aba6d4744d1bc96526e1fe2caea67
mosiahvuxs <moisessrocha@gmail.com>
2012-10-02 14:22:55 -0300

    .

--- src/br/com/zapeat/site/dao/PromocaoDAO.java
+++ src/br/com/zapeat/site/dao/PromocaoDAO.java
@@ -37,7 +37,7 @@ public class PromocaoDAO {
 
 		broker.setPropertySQL("promocaodao.obterPromocaoHora");
 
-		return (PromocaoModel) broker.getObjectBean(PromocaoModel.class, "id", "tipoPromocaoModel.id", "tipoPromocaoModel.descricao", "fornecedorModel.id", "fornecedorModel.nomeFantasia", "descricao", "inicio", "fim", "precoOriginal", "precoPromocional", "titulo", "fornecedorModel.longitude", "fornecedorModel.latitude", "imagemPromocao");
+		return (PromocaoModel) broker.getObjectBean(PromocaoModel.class, "id", "tipoPromocaoModel.id", "tipoPromocaoModel.descricao", "fornecedorModel.id", "fornecedorModel.nomeFantasia", "descricao", "inicio", "fim", "precoOriginal", "precoPromocional", "titulo", "fornecedorModel.longitude", "fornecedorModel.latitude", "imagemPromocao", "fornecedorModel.logoMarca");
 
 	}
 
